627067d0f05abb3d36b735404e337d4ab1b3b368
= <sjhyun0926@gmail.com>
2024-07-24 16:42:06 +0900

    initial commit

---
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/PillMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pill_Mate_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillMateBackendApplication.class, args);
+	}
+
+}
