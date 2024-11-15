6b59261907a4f95a189fd9575171a6d4c3a39ea1
sophiehan12 <sophiehan12@naver.com>
2024-11-15 23:55:06 +0900

    #10MedicineCheck완료

--- Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/check/controller/CheckController.java
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/check/controller/CheckController.java
@@ -1,20 +1,30 @@
 package com.example.Pill_Mate_Backend.domain.check.controller;
 
+import com.example.Pill_Mate_Backend.domain.check.dto.MedicineCheckDTO;
 import com.example.Pill_Mate_Backend.domain.check.dto.MedicineDTO;
 import com.example.Pill_Mate_Backend.domain.check.service.HomeService;
+import com.example.Pill_Mate_Backend.domain.check.service.MedicineCheckService;
+import com.example.Pill_Mate_Backend.domain.oauth2.controller.AuthController;
 import com.example.Pill_Mate_Backend.domain.oauth2.service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +34,8 @@ public class CheckController {
     @Autowired
     private HomeService homeService;
     private final JwtService jwtService;
+    private final MedicineCheckService medicineCheckService;
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @GetMapping("/default")
     public List<MedicineDTO> getMedicineSchedulesByDate(@RequestHeader(value = "Authorization", required = true) String token) {
@@ -48,5 +60,16 @@ public class CheckController {
         }
         System.out.print(homeService.getMedicineSchedulesByDate(email, mydate));
         return homeService.getMedicineSchedulesByDate(email, mydate);
+    }
+
+    @PatchMapping("/medicinecheck")
+    public ResponseEntity<?> updateMedicineCheck(@RequestBody List<MedicineCheckDTO> medicineCheckList) {
+        System.out.print(medicineCheckList);
+        if (medicineCheckList == null || medicineCheckList.isEmpty()) {
+            return ResponseEntity.badRequest().body("Invalid or empty request body");
+        }
+
+        medicineCheckService.updateCheckStatus(medicineCheckList);
+        return ResponseEntity.ok().build();
     }
 }
---
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/check/service/MedicineCheckService.java
@@ -0,0 +1,32 @@
+package com.example.Pill_Mate_Backend.domain.check.service;
+
+import com.example.Pill_Mate_Backend.CommonEntity.MedicineSchedule;
+import com.example.Pill_Mate_Backend.domain.check.dto.MedicineCheckDTO;
+import com.example.Pill_Mate_Backend.domain.check.repository.MedicineScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MedicineCheckService {
+    @Autowired
+    private MedicineScheduleRepository repository;
+
+    public void updateCheckStatus(List<MedicineCheckDTO> updateList) {
+        for (MedicineCheckDTO dto : updateList) {
+            MedicineSchedule schedule = repository.findById(dto.getMedicineScheduleId())
+                    .orElseThrow(() -> new ResourceNotFoundException("MedicineSchedule not found"));
+            schedule.setEatCheck(dto.getEatCheck());
+            repository.save(schedule);
+        }
+    }
+
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message) {
+            super(message);
+        }
+    }
+}
