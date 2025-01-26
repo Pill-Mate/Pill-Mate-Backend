e5fb34e13399ea711393680a3d174cc9a9e9135a
= <sjhyun0926@gmail.com>
2025-01-26 16:01:26 +0900

    [Fix] 병용금기 데이터 및 로직 수정 - 예외 처리 추가 - 약물 충돌 시 기존 약물 삭제 api 추가

--- Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/conflict/controller/DurApiController.java
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/conflict/controller/DurApiController.java
@@ -1,10 +1,16 @@
 package com.example.Pill_Mate_Backend.domain.conflict.controller;
 
+import com.example.Pill_Mate_Backend.domain.conflict.dto.PhoneAddresses;
 import com.example.Pill_Mate_Backend.domain.conflict.service.ApiService;
 import com.example.Pill_Mate_Backend.domain.conflict.service.EfcyApiService;
+import com.example.Pill_Mate_Backend.domain.register.repository.HospitalRepository;
+import com.example.Pill_Mate_Backend.domain.register.repository.MedicineRepository;
+import com.example.Pill_Mate_Backend.domain.register.repository.PharmacyRepository;
+import com.example.Pill_Mate_Backend.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,14 +27,22 @@ import java.net.URLEncoder;
 @RequestMapping("/api/v1/dur")
 @RequiredArgsConstructor
 public class DurApiController {
+    private final ApiService apiService;
+
+    @Autowired
+    private final MedicineRepository medicineRepository;
+
     @Value("${openApi.serviceKey}")
     private String serviceKey;
+
+
     // 병용금기
     @Operation(summary = "병용금기", description = "헤더의 itemSeq약물 번호로 해당 약물의 병용금기 약물 리턴")
     @GetMapping("/usjnt-taboo")
     public String UsjntTaboocallapi(@RequestParam String itemSeq) throws IOException {
         StringBuilder sb = new StringBuilder();
         //병용금기 정보조회
+        try {
         String urlbyeongyong = "http://apis.data.go.kr/1471000/DURPrdlstInfoService03/getUsjntTabooInfoList03?" +
                 "serviceKey="+ serviceKey +
                 //받아올 페이지 수
@@ -57,11 +71,19 @@ public class DurApiController {
         urlConnection.disconnect();
         String json = sb.toString(); // Replace with actual JSON
 
-        ApiService apiService = new ApiService();
+        ApiService apiService = new ApiService(medicineRepository);
         String resultJson = apiService.usjntTabooProcessApiItems(json);
 
         //return sb.toString();
-        return resultJson;
+            System.out.println(resultJson);
+            return resultJson;
+        } catch (Exception e) {
+            //나중에 responseBody 추가
+            System.out.println(e.getMessage());
+            return "실패";
+
+        }
+
 
     }
     //효능군 중복
@@ -69,6 +91,7 @@ public class DurApiController {
     @GetMapping("/efcy-dplct")
     public String EfcyDplctcallapi(@RequestParam String itemSeq) throws IOException {
         StringBuilder sb = new StringBuilder();
+        try {
         //병용금기 정보조회
         String urlEfcy = "http://apis.data.go.kr/1471000/DURPrdlstInfoService03/getEfcyDplctInfoList03?" +
                 "serviceKey="+ serviceKey +
@@ -99,13 +122,42 @@ public class DurApiController {
         urlConnection.disconnect();
         String json = sb.toString(); // Replace with actual JSON
 
-        EfcyApiService efcyApiService = new EfcyApiService();
+        EfcyApiService efcyApiService = new EfcyApiService(medicineRepository);
         String resultJson = efcyApiService.efcyDplctProcessApiItems(json);
 
         //return sb.toString();
+            System.out.println(resultJson);
         return resultJson;
+    } catch (Exception e) {
+        //나중에 responseBody 추가
+        return "효능군 중복 리턴 실패";
+
+    }
 
 
+    }
+
+    @Operation(summary = "약국 병원 이름 주소 전화번호 ", description = "약국 병원 이름 주소 전화번호 리턴")
+    @GetMapping("get-phone-address")
+    public ApiResponse<?> getPhoneNumber(@RequestParam String itemSeq) throws IOException {
+    try {
+
+        PhoneAddresses phoneAddresses = apiService.getPhoneAddresses(itemSeq);
+        return ApiResponse.onSuccess(phoneAddresses);
+    } catch (Exception e) {
+        return ApiResponse.onFailure(e.getMessage());
+    }
+
+    }
+    @DeleteMapping("/conflict-remove")
+    public ApiResponse<?> deleteConflict(@RequestParam String itemSeq) throws IOException {
+        //충돌 실제 약물 넣어놓을 시에 추가
+        apiService.delete(itemSeq);
+        try {
+            return ApiResponse.onSuccess("성공");
+        } catch (Exception e) {
+            return ApiResponse.onFailure(e.getMessage());
+        }
     }
 
 }
