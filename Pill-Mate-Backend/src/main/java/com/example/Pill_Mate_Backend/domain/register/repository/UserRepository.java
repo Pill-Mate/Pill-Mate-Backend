9f52b9754bb4ff66f56713ae9b6730e19c25c5b8
= <sjhyun0926@gmail.com>
2025-02-23 18:04:46 +0900

    [Fix] 로그인 방식 수정 - 복용중 약물리스트 - 복용 중지 약물 리스트

--- Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/register/repository/UserRepository.java
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/register/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.Pill_Mate_Backend.domain.register.repository;
 
 import com.example.Pill_Mate_Backend.CommonEntity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
-    Optional<Long> findIdxByEmail(String email);
+    @Query("SELECT u.id FROM Users u WHERE u.email = :email")
+    Optional<Long> findIdxByEmail(@Param("email") String email);
 }
