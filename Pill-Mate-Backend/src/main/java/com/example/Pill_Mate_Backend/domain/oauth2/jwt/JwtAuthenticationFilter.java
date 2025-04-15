797da14d1eafa7a28a3026c1e694f7c3a3617815
sophiehan12 <sophiehan12@naver.com>
2025-04-16 00:52:54 +0900

    #6Oauth db삭제 jwt 에러 수정_마지막

--- Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/oauth2/jwt/JwtAuthenticationFilter.java
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/oauth2/jwt/JwtAuthenticationFilter.java
@@ -87,7 +87,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         } else {
             System.out.println("Authorization header is missing or invalid");
         }
-        System.out.println("jwt필터 실행됨");
         filterChain.doFilter(request, response);
     }
 }
