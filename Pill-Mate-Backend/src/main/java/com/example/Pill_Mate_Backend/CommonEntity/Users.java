fabe04a3c9c6342cf1b96f62d4e61d2064168d25
sophiehan12 <sophiehan12@naver.com>
2025-08-02 21:21:10 +0900

    #77 appleid 길이 수정

--- Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/CommonEntity/Users.java
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/CommonEntity/Users.java
@@ -21,7 +21,7 @@ public class Users extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = true, unique = true, length = 100)
+    @Column(nullable = true, unique = true, length = 255)
     private String appleId;
 
     @Column(nullable = false, length = 50)
