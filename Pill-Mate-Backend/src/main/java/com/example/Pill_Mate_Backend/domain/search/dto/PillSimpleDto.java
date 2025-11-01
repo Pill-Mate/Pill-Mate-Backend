ab9a9b902cf45995dd20c6de3184eda843d77ecf
= <sjhyun0926@gmail.com>
2025-11-01 19:41:26 +0900

    [Feat] 약물 상세 검색 구현 - 약물 상세 페이지에서 검색해서 해당 페이지 상세 정보 볼때 사용

---
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/search/dto/PillSimpleDto.java
@@ -0,0 +1,12 @@
+package com.example.Pill_Mate_Backend.domain.search.dto;
+
+public interface PillSimpleDto {
+    String getItemName();
+    String getItemImage();
+    String getEntpName();
+    String getClassName();
+    String getUseMethodQesitm();
+    String getEfcyQesitm();
+    String getAtpnQesitm();
+    String getDepositMethod();
+}
