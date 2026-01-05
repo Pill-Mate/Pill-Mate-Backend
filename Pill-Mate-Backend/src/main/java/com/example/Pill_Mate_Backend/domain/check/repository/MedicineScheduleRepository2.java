e90059806c8eff4073e8e20d2ad23b5ed2ed201b
= <sjhyun0926@gmail.com>
2026-01-05 16:59:13 +0900

    [refractor] 약물 식별 번호 타입 통일 3

--- Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/check/repository/MedicineScheduleRepository2.java
+++ Pill-Mate-Backend/src/main/java/com/example/Pill_Mate_Backend/domain/check/repository/MedicineScheduleRepository2.java
@@ -24,7 +24,7 @@ public interface MedicineScheduleRepository2 extends JpaRepository<MedicineSched
                 ms.meal_unit AS mealunit,
                 ms.eat_check AS eatcheck,
                 m.medicine_name AS medicinename,
-                m.identify_number As itemseq,
+                m.item_seq As itemseq,
                 m.medicine_image AS medicineimage
             FROM medicine_schedule ms
             JOIN medicine m ON ms.medicine_id = m.id
